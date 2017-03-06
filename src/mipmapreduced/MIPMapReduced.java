34e5e745f2e266e65b22300b559f95804c5b25bf
Ioannis Xarchakos <zannisxarchakos@hotmail.com>
2017-03-06 20:36:31 +0200

    [DEV] unpivot functionality

--- src/mipmapreduced/MIPMapReduced.java
+++ src/mipmapreduced/MIPMapReduced.java
@@ -1,6 +1,6 @@
 package mipmapreduced;
 
-import static org.apache.commons.io.FilenameUtils.separatorsToSystem;
+import org.apache.commons.io.FilenameUtils;
 
 public class MIPMapReduced {
 
@@ -9,43 +9,63 @@ public class MIPMapReduced {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try{        
-            if (args.length > 4) {
-                printWrongInputMessage();
-            }                
-            else {                 
-                String absoluteMappingTaskFilepath = separatorsToSystem(args[0]);
-                String exportPath = separatorsToSystem(args[1]);
-                String dbConfFile = separatorsToSystem(args[2]);
-                boolean pkConstraints = true;
-                if (args.length == 4){                
-                    String pk = args[3];
-                    if (pk.equalsIgnoreCase("t") || pk.equalsIgnoreCase("true"))
-                        pkConstraints = true;
-                    else if (pk.equalsIgnoreCase("f") || pk.equalsIgnoreCase("false"))
-                        pkConstraints = false;
-                    else 
-                        System.out.println("Last parameter must be either \"t\" (true) or \"f\" (false). Default value is true.");
-                }                
-
-                DirectoryChecker checker = new DirectoryChecker();
-                if (checker.checkFileValidity(absoluteMappingTaskFilepath) 
-                    && checker.checkDirectoryValidity(exportPath)
-                    && checker.checkFileValidity(dbConfFile)) 
-                {            
-                    DbConnector dbconnect = new DbConnector();        
-                    dbconnect.configureDatabaseProperties(dbConfFile); 
-
-                    TaskHandler handleMappingTask = new TaskHandler(absoluteMappingTaskFilepath, exportPath, pkConstraints);
-                    handleMappingTask.performAction(); 
+        try{     
+            if (args[0].equals("-unpivot")){
+                if (args.length != 5) {
+                    printWrongInputMessage("unpivot");
+                }else {
+                    String inputPath = FilenameUtils.separatorsToSystem(args[1]);
+                    String dbConfFile = FilenameUtils.separatorsToSystem(args[2]);
+                    String newColName = args[3];
+                    String[] columns = args[4].split(",");
+                    DirectoryChecker checker = new DirectoryChecker();
+                    if (checker.checkFileValidity(inputPath) && checker.checkFileValidity(dbConfFile))
+                    { 
+                        DbConnector dbconnect = new DbConnector();        
+                        dbconnect.configureDatabaseProperties(dbConfFile); 
+                        TaskHandler handleMappingTask = 
+                                new TaskHandler(inputPath, columns, newColName);
+                        handleMappingTask.unPivot();
+                    } else {
+                        System.out.println("\nInvalid path input or the file/path does not exist: " + checker.getInvalidFilePath());
+                    }
                 }
-                else {
-                    System.out.println("\nInvalid path input or the file/path does not exist: " + checker.getInvalidFilePath());
+            } else {
+                if (args.length > 4) {
+                    printWrongInputMessage("translate");
+                }                
+                else {                 
+                    String absoluteMappingTaskFilepath = FilenameUtils.separatorsToSystem(args[0]);
+                    String exportPath = FilenameUtils.separatorsToSystem(args[1]);
+                    String dbConfFile = FilenameUtils.separatorsToSystem(args[2]);
+                    boolean pkConstraints = true;
+                    if (args.length == 4){                
+                        String pk = args[3];
+                        if (pk.equalsIgnoreCase("t") || pk.equalsIgnoreCase("true"))
+                            pkConstraints = true;
+                        else if (pk.equalsIgnoreCase("f") || pk.equalsIgnoreCase("false"))
+                            pkConstraints = false;
+                        else 
+                            System.out.println("Last parameter must be either \"t\" (true) or \"f\" (false). Default value is true.");
+                    }                
+
+                    DirectoryChecker checker = new DirectoryChecker();
+                    if (checker.checkFileValidity(absoluteMappingTaskFilepath) 
+                        && checker.checkDirectoryValidity(exportPath)
+                        && checker.checkFileValidity(dbConfFile)) 
+                    {            
+                        DbConnector dbconnect = new DbConnector();        
+                        dbconnect.configureDatabaseProperties(dbConfFile); 
+
+                        TaskHandler handleMappingTask = new TaskHandler(absoluteMappingTaskFilepath, exportPath, pkConstraints);
+                        handleMappingTask.performAction(); 
+                    }
+                    else {
+                        System.out.println("\nInvalid path input or the file/path does not exist: " + checker.getInvalidFilePath());
+                    }
                 }
             }
-        } 
-        catch (ArrayIndexOutOfBoundsException ex){
-            printWrongInputMessage();
+            
         }        
         catch (Exception ex){
             System.out.println(ex);
@@ -53,12 +73,22 @@ public class MIPMapReduced {
         }        
     }
     
-    private static void printWrongInputMessage() {        
-        System.out.println("\nWrong input. For correct MIPMapReduced usage run jar as:");
-        System.out.println("java -jar <Path To Jar>/MIPMapReduced.jar <Path To Mapping Task File/Mapping Task File.xml> "
-                + "<Path To Export Translated Instances> "
-                + "<Path To Database Configuration File/Database Configuration File> "
-                + "(true/false export possible Primary Key violations - optional)");
+    private static void printWrongInputMessage(String option) {    
+        if(option.equals("unpivot")){
+            System.out.println("\nWrong input. For correct MIPMapReduced usage run jar as:");
+            System.out.println("java -jar <Path To Jar>/MIPMapReduced.jar "
+                    + "-unpivot "
+                    + "<Path To Mapping Csv File/SomeFile.csv> "
+                    + "<new column name> "
+                    + "<columns that include in pivoting, comma separated or keepall to keep all columns> ");
+        } else if (option.equals("translate")){
+            System.out.println("\nWrong input. For correct MIPMapReduced usage run jar as:");
+            System.out.println("java -jar <Path To Jar>/MIPMapReduced.jar <Path To Mapping Task File/Mapping Task File.xml> "
+                    + "<Path To Export Translated Instances> "
+                    + "<Path To Database Configuration File/Database Configuration File> "
+                    + "(true/false export possible Primary Key violations - optional)");
+        }
+        
     }
     
 }
--- src/mipmapreduced/TaskHandler.java
+++ src/mipmapreduced/TaskHandler.java
@@ -4,20 +4,69 @@ import it.unibas.spicy.model.mapping.MappingTask;
 import it.unibas.spicy.persistence.DAOException;
 import it.unibas.spicy.persistence.DAOMappingTask;
 import it.unibas.spicy.persistence.csv.DAOCsv;
+import it.unibas.spicy.persistence.csv.UnpivotCSVDAO;
 import it.unibas.spicy.utility.SpicyEngineConstants;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TaskHandler {
     private String fileAbsoluteFile;
-    private String targetPath;
+    private String targetPath, newColName;
     private boolean pkConstraints;
-        
+    private String[] selectedColumns;
     public TaskHandler(String fileAbsoluteFile, String targetPath, boolean pkConstraints) {
         this.fileAbsoluteFile = fileAbsoluteFile;
         this.targetPath = targetPath;
         this.pkConstraints = pkConstraints;
+        
     }
+    
+    public TaskHandler(String fileAbsoluteFile, String[] selectedColumns, String newColName) {
+        this.fileAbsoluteFile = fileAbsoluteFile;
+        this.selectedColumns = selectedColumns;
+        this.newColName = newColName;
+    }
+    
+    public void unPivot() throws DAOException, SQLException, IOException{
+        ArrayList<String> colNames = new ArrayList();
+        ArrayList<String> keepColNames = new ArrayList();
+        try {
+            UnpivotCSVDAO daoUnpivot = new UnpivotCSVDAO();
+            File file = new File(fileAbsoluteFile);
+            String[] columnNames = daoUnpivot.getCsvTableColumns(file);
+            //we keep all columns for pivoting
+            if (selectedColumns.length == 1 && selectedColumns[0].equalsIgnoreCase("keepall")){
+                for(String col: columnNames){
+                    colNames.add(col);
+                }
+            } else {
+                for(String col: columnNames){
+                    if (!Arrays.asList(selectedColumns).contains(col)){
+                        keepColNames.add(col);
+                    } else {
+                        colNames.add(col);
+                    }
+                }
+            }
+            for(String item: colNames){
+                System.out.println(item);
+            }
+            System.out.println("---");
+            for(String item: keepColNames){
+                System.out.println(item);
+            }
+            daoUnpivot.unpivotTable(keepColNames, colNames, newColName, file);
+        } catch (IOException ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+        } 
+    }
+    
     
     public void performAction() {
         DAOMappingTask daoMappingTask = new DAOMappingTask();
